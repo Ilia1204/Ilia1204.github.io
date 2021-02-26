1e10efcc61a445e1353d655af71f395815307d1d
Ilia1204 <lower013634@gmail.com>
2021-02-26 18:36:02 +0400

    version 2.1
    version 2.1

--- FormInUp/php/send.php
+++ FormInUp/php/send.php
@@ -1,39 +1,189 @@
-<?php
-/* Здесь проверяется существование переменных */
-if (isset($_POST['name'])) {$name = $_POST['name'];}
-if (isset($_POST['surname'])) {$surname = $_POST['surname'];}
-if (isset($_POST['email'])) {$email_user = $_POST['email-user'];}
-if (isset($_POST['userid'])) {$userid = $_POST['userid'];}
-if (isset($_POST['password'])) {$password = $_POST['password'];}
-if (isset($_POST['cor-password'])) {$cor_password = $_POST['cor-password'];}
- 
-/* Сюда впишите свою эл. почту */
-$myaddres  = "lower013634@gmail.com"; // кому отправляем
- 
-/* А здесь прописывается текст сообщения, \n - перенос строки */
-$mes = "Тема: Регистрация нового пользователя!\nИмя: $name\nФамилия: $surname\nEmail: $email_user\nНикнейм: $userid\nПароль: $password\nПотверждение пароля: $cor_password";
- 
-/* А эта функция как раз занимается отправкой письма на указанный вами email */
-$sub='Заказ'; //сабж
-$email='Заказ обратного звонка'; // от кого
-$send = mail ($myaddres,$sub,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
- 
-ini_set('short_open_tag', 'On');
-header('Refresh: 3; URL=index.html');
-?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<meta http-equiv="refresh" content="3; url=index.html">
-<title>Спасибо! Мы свяжемся с вами!</title>
-<meta name="generator">
-<script type="text/javascript">
-setTimeout('location.replace("/index.html")', 3000);
-/*Изменить текущий адрес страницы через 3 секунды (3000 миллисекунд)*/
-</script> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Форма регистрации</title>
 </head>
 <body>
-<h1>Спасибо! Мы свяжемся с вами!</h1>
+    <?php
+    //проверяем, существуют ли переменные в массиве POST
+        if(!isset($_POST['email']) and !isset($_POST['password'])){
+ ?>
+    <div class="header" id="myheader">
+        <h1 class="header-form-sign-in" data-aos="fade-right">
+            <span>
+             Form
+            </span>
+            Sign In
+        </h1>
+
+        <nav class="menu">
+            <ul class="menu-lists">
+                <li class="lists-item" data-aos="fade-down-right">
+                    <a href="#person">Home</a>
+                </li>
+
+                <li class="lists-item"data-aos="fade-down-left">
+                    <a href="#about">About</a>
+                </li>
+
+                <li class="lists-item"data-aos="fade-down-right">
+                    <a href="#services">Services</a>
+                </li>
+
+                <li class="lists-item"data-aos="fade-down-left">
+                    <a href="#blog">Blog</a>
+                </li>
+
+                <li class="lists-item"data-aos="fade-down-right">
+                    <a href="#portfolio">Portfolio</a>
+                </li>
+
+                <li class="lists-item"data-aos="fade-down-left">
+                    <a href="#contact">Contact</a>
+                </li>
+
+                <li class="lists-item"data-aos="fade-down-right">
+                    <a href="#privacy-policy">Privacy policy</a>
+                </li>
+            </ul>
+        </nav>
+        <button class="button1"  data-aos="fade-left">Contact Me</button>
+        <ul class="social-links">
+            <li>
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                    <i class="fa fa-bars">
+                    </i>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="container">
+        <form class="form" action="send.php" method="post">
+            <div class="action">
+                <div class="dws-input">
+                    <i class="fa fa-user" aria-hidden="true"></i>
+                    <input type="email" name="email" placeholder="Email Address"  title = "Введите ваш email" maxlength="35"required>
+                </div>
+                <div class="dws-input2">
+                    <i class="fa fa-lock" aria-hidden="true"></i>
+                    <input type="password"  id="password-input" class = "password" name="password" placeholder="Password"maxlength="25" title = "Введите ваш пароль" required><br />
+                    <label class="password-control"></label>
+              </form>   
+
+                </div> 
+                <div class="buttons">
+                    <div class="btn">
+                        <input class="button" type="submit" name="submit" value="Sign In" title="Войти"><br />
+                    </div>
+                        <input onClick="window.location.href='register-form.html'" class="button" type="submit" name="submit" value="Sign Up" title="Зарегистрироваться"><br />
+                </div>
+                <div class="group">
+                    <input type="hidden" name="isPermanent" value="0">
+                    <input id="check" name="isPermanent" value="1" type="checkbox" class="check" checked>
+                    <label for="check">
+                        <span class="icon">
+                        </span>
+                        <span class="remember_me">Remember me</span>
+                    </label>
+                </div>
+                <div class="forgot">
+                    <a href="ForgotPass.html" title="Забыли пароль" class="forgot_a_pass">
+                       Forgot <span class="forgot-pass">password?</span>
+                    </a>
+                </div>
+                <ul class="social-links" >
+                    <li title="Мы в facebook">
+                        <a href="#">
+                            <img src="./img/social/facebook-white.svg" alt="facebook" >
+                        </a>
+                    </li>
+                    
+                    <li title="Мы в twitter">
+                        <a href="#">
+                            <img src="./img/social/twitter-white.svg" alt="twitter" >
+                        </a>
+                    </li>
+        
+                    <li title="Мы в pinterest">
+                        <a href="#">
+                            <img src="./img/social/pinterest-white.svg" alt="pinterest" >
+                        </a>
+                    </li>
+        
+                    <li title="Мы в linkedin">
+                        <a href="#">
+                            <img src="./img/social/linkedin-white.svg" alt="linkedin">
+                        </a>
+                    </li>
+        
+                    <li title="Мы в instagram">
+                        <a href="#">
+                            <img src="./img/social/instagram-white.svg" alt="instagram" >
+                        </a>
+                    </li>
+                </ul>
+            </div> 
+       
+            <div class="form-with-img" title="Martian"></div>
+    </div>    
+    
+        <script src="./js/jquery-3.5.1.min.js"></script>
+        <script src="./js/index.js"></script> 
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>  
+        <script type="./js/jquery.cookie.js"></script>
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+        <script>
+            AOS.init();
+        </script>
+        <script>
+            function myFunction() {
+                var x = document.getElementById("myheader");
+                    if (x.className === "header") {
+                        x.className += " responsive";
+                    } 
+                    else {
+                        x.className = "header";
+                    }
+            }
+        </script>
+        <script>history.scrollRestoration = "manual"</script>
+    
+    
+    
+    <?php
+    } else {
+        //показываем форму
+    
+    $email = $_POST['email'];  //Создаём две переменный, так как два инпута
+    $password = $_POST['password'];
+    
+    $email = htmlspecialchars($email); //преобразуем все символы, которые пользователь попытается добавить в форму:
+    $password = htmlspecialchars($password);
+
+    $email= urldecode($email); //декодируем url, если пользователь попытается его добавить в форму.
+    $password = urldecode($password);
+
+    $email = trim($email); //удаляем пробелы с начала и конца строки, если таковые имеются:
+    $password = trim($password);
+
+    //echo $email; //Проверяли что выводилось
+    //echo "<br>";
+    //echo $password;
+
+        if (mail("lower013634@gmail.com", "Внимание! Пользователь совершил вход на сайт Martian.", "Email-
+        адрес пользователя: ".$email.". Пароль: ".$password ,"From: temail-test@yandex.ru \r\n"))
+        {
+            echo 'Сообщение успешно отправлено!';
+        }
+        else {
+            echo 'При отправке данного сообщения возникли ошибки.'
+        }
+    }
+    ini_set('display_errors','On');
+    error_reporting('E_ALL');
+?>
 </body>
 </html>
+
