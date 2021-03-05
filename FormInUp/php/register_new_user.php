f68b73f50b262f3ea5c504b2ac7883d84db25001
Ilia1204 <lower013634@gmail.com>
2021-03-05 17:45:33 +0400

    version 2.4
    version 2.4

--- FormInUp/php/register_new_user.php
+++ FormInUp/php/register_new_user.php
@@ -1,8 +1,8 @@
-    <?php
+<?php
         //require 'connection.php';
 
 
-        //проверяем, существуют ли переменные в массиве POST
+        //проверяем, существуют ли переменные в массиве POST, т.е. если они не существуют в массиве ( ! )
         if(!isset($_POST['name']) and !isset($_POST['surname'])  and !isset($_POST['email_user']) and !isset($_POST['user_id']) and !isset($_POST['password']))
         {
         
@@ -10,23 +10,34 @@
         else 
         {
             
+          /* $sql = "SELECT * FROM `users_register` WHERE `id_string` = '$user_id'"; 
+                if ($result1 = mysqli_query($sql)) {
+                    if(mysql_num_rows($result1) > 0) {
+                        echo'User already exists';
+                        exit;
+                    }
+                }*/
+
+
+            $salt = "Some random string";
+
+
 
             $host = 'localhost'; // адрес сервера 
             $database = 'users'; // имя базы данных
             $user = 'root'; // имя пользователя
             $password1 = 'root'; // пароль
-            $table = 'users_register';
+            $table = 'users_register'; //название таблицы
 
             
-          
-            
-
             //показываем форму
             $name = $_POST['name'];  //Создаём 5 переменныx, так как 6 инпутов, но один повторяется - пароль
             $surname = $_POST['surname'];
             $email_user = $_POST['email_user']; 
             $user_id = $_POST['user_id'];
             $password = $_POST['password']; 
+            $password2 = $_POST['password'];
+            
             
             $name = $_REQUEST['name'];  //Создаём 5 переменныx, так как 6 инпутов, но один повторяется - пароль
             $surname = $_REQUEST['surname'];
@@ -52,19 +63,14 @@
             $user_id = trim($user_id);
             $password = trim($password);
 
-
-
-                /* $insert_sql = "INSERT INTO users (name, surname, email_user, user_id, password)" .
-                        "VALUES('{$name}', '{$surname}', '{$surname}', '{$email_user}','{$user_id}','{$password}');";
-                                mysql_query($insert_sql);*/
-
-                try {
+               try {
+                        $password = md5($salt.$password);
                         // Подключение к базе данных
                         $db = new PDO("mysql:host=$host;dbname=$database", $user, $password1);
                         // Устанавливаем корректную кодировку
                         $db->exec("set names utf8");
                         // Собираем данные для запроса
-                        $data = array( 'name' => $name, 'surname' => $surname, 'email_user' => $email_user, 'user_id' => $user_id, 'password' => $password  ); 
+                        $data = array( 'name' => $name, 'surname' => $surname, 'email_user' => $email_user, 'user_id' => $user_id, 'password' => $password); 
                         // Подготавливаем SQL-запрос
                         $query = $db->prepare("INSERT INTO `users_register` (`id`, `name`, `surname`, `email`, `id_string`, `pass`, `con_pass`) VALUES (NULL, '$name', '$surname', '$email_user', '$user_id', '$password', '$password');");
                         // Выполняем запрос с данными
@@ -81,21 +87,23 @@
                     }
             
 
-
-
-            /*if (md5($password) === '1afa148eb41f2e7103f21410bf48346c') { 
-               $password = "***********";
-             }*/
-                
-
-                if (mail("lower013634@gmail.com", "Внимание! Регистрация нового пользователя на сайте Martian.", "Имя пользователя: ". $name . "." . "\r\n" . "Фамилия: " . $surname . "." . "\r\n" . "Email пользователя: " . $email_user . "." . "\r\n" . "Идентификатор пользователя: " . $user_id . "." . "\r\n" . "Пароль: " . $password . "." . "\r\n" . "Подтверждение пароля: " . $password . ".". "\r\n" , "From: mail.temp.test12@gmail.com"))
-                {
-                    echo 'Вы успешно зарегистрировались! Подтвердите пожалуйста вашу почту: ' . $email_user . ".";
-                }
-                else {
-                    echo ' При отправке данного сообщения возникли ошибки.';
-                }
-              }  
-    ini_set('display_errors','On');
-    error_reporting('E_ALL');
+                    if (mail("lower013634@gmail.com", "Внимание! Регистрация нового пользователя на сайте Martian.", "Имя пользователя: ". $name . "." . "\r\n" . "Фамилия: " . $surname . "." . "\r\n" . "Email пользователя: " . $email_user . "." . "\r\n" . "Идентификатор пользователя: " . $user_id . "." . "\r\n" . "Пароль: " . $password . "." . "\r\n" . "Подтверждение пароля: " . $password . ".". "\r\n" , "From: mail.temp.test12@gmail.com"))
+                    {
+                        echo 'Вы успешно зарегистрировались! Подтвердите пожалуйста вашу почту: ' . $email_user . "." . "<br />" ;
+                    }
+                    else {
+                        echo ' При отправке данного сообщения возникли ошибки.';
+                    }
+                  
+                    //Отправляем письмо на емэйл пользователя, но пароль пользователю отправляем не хэшируемый!
+                    if (mail($email_user, "Вы успешно зарегистрировались на сайте Martian.", "Ваш Email: ". $email_user . "." . "\r\n" . "Пароль: ". $password2 . ".". "\r\n" . "\r\n" . "Если это были не вы, рекомендуем вам сменить пароль.", "From: mail.temp.test12@gmail.com"))
+                    {
+                        echo 'Проверьте письмо с регистрацией!.';
+                    }
+                    else {
+                        echo 'При отправке данного письма возникли ошибки.';
+                    }
+        }  
+            ini_set('display_errors','On'); //показываем все ошибки, если они будут.
+            error_reporting('E_ALL');
 ?>
