ff7d778d5d6d187305163fa59bb0b29448e3f913
Ilia1204 <74100426+Ilia1204@users.noreply.github.com>
2021-03-25 20:30:48 +0400

    version 4.2
    version 4.2

--- Admin/js/index.js
+++ Admin/js/index.js
@@ -22,3 +22,16 @@
             $(this).hide();
         }
     });
+
+     $('.header-button').hover(function() {
+       $('div.dropdown', this).slideDown(500);    
+   },function() {
+       $('div.dropdown', this).slideUp(500);             
+   });
+
+   $('.hamburger').click(function() {
+        $('span:nth-child(1)').toggleClass('first_line');
+        $('span:nth-child(2)').toggleClass('second_line');
+        $('span:nth-child(3)').toggleClass('third_line');
+        $('.menu_top').toggleClass('menu_top_active');
+    });
