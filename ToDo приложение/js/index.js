640ab77cc6b3ff3e1346d2c3b5f02697998c85fd
Ilia1204 <74100426+Ilia1204@users.noreply.github.com>
2021-04-08 12:13:51 +0400

    version 1.1
    version 1.1

--- ToDo приложение/js/index.js
+++ ToDo приложение/js/index.js
@@ -1,20 +1,17 @@
 $(function () {
 	let div = $('div')
-
-	function addTask(title) {
+	let titleElem = $('.name')
+	$('.add_tasks').click(function addTask(titleElem) {
 		// подставляем данные которые пришли с формы
 		let task = `
       <div class="todo-item">
 				<input type="checkbox" class="checkbox">
 				<span class="name task-1">${title}</span>
-			</div>
-
-        <input type="text" placeholder="Название">${title}
+			</div>	
         `
 		// добавляем нашу задачку и скрываем p, в котором находился текст 'Список пуст...'
 		div.append(task)
-	}
-
+	})
 	$('.checkbox').click(
 		function () {
 			$('.task-1').attr('style', 'text-decoration: none;')
