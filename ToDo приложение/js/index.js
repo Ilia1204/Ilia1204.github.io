0dc637cd5e9e1b4f1b6472dbf89b634ba44f8a52
Ilia1204 <74100426+Ilia1204@users.noreply.github.com>
2021-04-09 12:19:43 +0400

    version 1.2
    version 1.2

--- ToDo приложение/js/index.js
+++ ToDo приложение/js/index.js
@@ -1,67 +1,64 @@
-$(function () {
-	let ul = $('ul')
-	let titleElem = $('.name')
-	let input = $('.input_name')
-	$('.add_tasks').click(function addTask(title) {
-		// подставляем данные которые пришли с формы
-		let task = `
-			<li>
-				<input type="checkbox">
-				<span>${title}</span>	
-    	</li>    
-			`
-		// добавляем нашу задачку
-		ul.append(task)
-		document.getElementById('input_name').value = document.getElementById(
-			'input_name'
-		).defaultValue
-	})
+let List = $('#ToDoApp ul')
+let Mask = 'tdl_'
 
-	input.submit(function (event) {
-		// делаем так чтобы форма не отправляла данные
-		event.preventDefault()
-
-		// собираем данные с полей
-		const title = titleElem.val()
-
-		// приводим форму к первоночальному виду
-
-		// вызываем нашу написанную функцию и отправляем ей данные взятые из формы
-		addTask(title)
-	})
-
-	$('.item-1').click(
-		function () {
-			$('.task-1').attr('style', 'text-decoration: none;')
-		},
-		function () {
-			$('.task-1').attr('style', 'text-decoration: line-through;')
+function showTasks() {
+	//	Узнаём размер хранилища
+	let Storage_size = localStorage.length
+	// Если в хранилище что-то есть…
+	if (Storage_size > 0) {
+		// то берём и добавляем это в задачи
+		for (let i = 0; i < Storage_size; i++) {
+			let key = localStorage.key(i)
+			if (key.indexOf(Mask) == 0) {
+				//	и делаем это элементами списка
+				$('<li title="Нажмите, чтобы удалить"></li>')
+					.addClass('tdItem')
+					.attr('data-itemid', key)
+					.text(localStorage.getItem(key))
+					.appendTo(List)
+			}
 		}
-	)
-	$('.todo-item').click(function () {
-		$(this).find(':checkbox').attr('checked', 'checked')
-	})
+	}
+}
+showTasks()
 
-	$('li').click(
-		function () {
-			$('.task-1').attr('style', 'text-decoration: none;')
-		},
-		function () {
-			$('.task-1').attr('style', 'text-decoration: line-through;')
-		}
-	)
-	$('.item-2').click(
-		function () {
-			$('.task-2').attr('style', 'text-decoration: none;')
-		},
-		function () {
-			$('.task-2').attr('style', 'text-decoration: line-through;')
-		}
-	)
+$('#input_name').on('keydown', function (e) {
+	if (e.keyCode != 13) return
+	let str = e.target.value
+	e.target.value = ''
+	// Если в поле ввода было что-то написано — начинаем обрабатывать
+	if (str.length > 0) {
+		let number_Id = 0
+		List.children().each(function (index, el) {
+			let element_Id = $(el).attr('data-itemid').slice(4)
+			if (element_Id > number_Id) number_Id = element_Id
+		})
+		number_Id++
+		// Отправляем новую задачу сразу в память
+		localStorage.setItem(Mask + number_Id, str)
+		// и добавляем её в конец списка
+		$('<li title="Нажмите, чтобы удалить"></li>')
+			.addClass('tdItem')
+			.attr('data-itemid', Mask + number_Id)
+			.text(str)
+			.appendTo(List)
+	}
+})
 
-	$('.delete_tasks').click(function () {
-		// написать удаление при нажатие на кнопку delete
-		let li = 'li'
-		$(li).remove()
-	})
+$(document).on('click', '.tdItem', function (event) {
+	// Находим задачу, по которой кликнули
+	let note = $(event.target)
+	// Убираем её из памяти
+	localStorage.removeItem(note.attr('data-itemid'))
+	// и убираем её из списка
+	note.remove()
+})
+
+$('.delete_tasks').click(function () {
+	const allNotes = $('.tdItem')
+	$(allNotes).remove()
+
+	if (allNotes.length == 0) {
+		console.log('Нечего удалять')
+	}
 })
