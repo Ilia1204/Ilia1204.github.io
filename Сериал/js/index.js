d924026cdf9066212fc4016f6d0c0dd896c219cf
Ilia1204 <74100426+Ilia1204@users.noreply.github.com>
2021-04-16 23:34:31 +0400

    version 1.8
    version 1.8

--- Сериал/js/index.js
+++ Сериал/js/index.js
@@ -1,33 +1,35 @@
 $(function () {
-	let viewport = $('.viewport').width() // сохраняем ширину всего блока с котором находится наша галерея
-	let slider = $('ul.slider')
-	let viewSliders = $('.dot')
-	let viewSlide = 0
-
-	$('.next').click(function () {
-		$(viewSliders[viewSlide]).removeClass('active') // удаляем класс у первой картинки так как она больше не активная
-		if (viewSlide < 4) {
-			viewSlide++
-		} else {
-			viewSlide = 0
-		}
-		$(viewSliders[viewSlide]).addClass('active') // добавляем активный класс для точки, чтобы понимать на какой мы картинке сейчас находимся
-		slider.animate({ left: -viewSlide * viewport + 'px' }, { duration: 500 }) // делаем сдвиг влево на ширину картинки
+	$('.button_films').click(function () {
+		$('.modal').show()
+		console.log('Открытие модального окна')
 	})
-	$('.prev').click(function () {
-		$(viewSliders[viewSlide]).removeClass('active')
 
-		if (viewSlide > 0) {
-			viewSlide--
-		} else {
-			viewSlide = 4
-		}
-
-		$(viewSliders[viewSlide]).addClass('active')
-		slider.animate({ left: -viewSlide * viewport + 'px' }, { duration: 500 })
+	$('.close').click(function () {
+		$('.modal').hide()
 	})
-	const btnOff = $('.scroll-off'),
-		btnOn = $('.scroll-on'),
-		main = $('body'),
-		landing = $(window)
+
+	$('.modal').click(function (event) {
+		console.log(event.target)
+
+		if (event.target == this) {
+			$(this).hide()
+		}
+	})
+
+	function filePreview(input) {
+		if (input.files && input.files[0]) {
+			var reader = new FileReader()
+			reader.onload = function (e) {
+				$('#uploadForm + embed').remove()
+				$('#uploadForm').after(
+					'<embed src="' + e.target.result + '" width="450" height="300">'
+				)
+			}
+			reader.readAsDataURL(input.files[0])
+		}
+	}
+
+	$('#file').change(function () {
+		filePreview(this)
+	})
 })
